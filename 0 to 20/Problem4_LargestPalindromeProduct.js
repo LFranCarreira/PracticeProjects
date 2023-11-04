1a7dac5b5e529f2a5bf560f4c6edc6ff87710921
Franco Carreira <carreirafranco@gmail.com>
2023-11-03 23:32:08 -0300

    Solved Project Euler Problem 4: Largest palindrome product

--- 0 to 20/Problem4_LargestPalindromeProduct.js
+++ 0 to 20/Problem4_LargestPalindromeProduct.js
@@ -3,7 +3,24 @@
 // Find the largest palindrome made from the product of two n-digit numbers.
 
 function largestPalindromeProduct(n) {
-  return true;
+  const min = Math.pow(10, n - 1);
+  const max = Math.pow(10, n) - 1;
+  let maxPalindrome = 0;
+
+  for (let i = max; i >= min; i--) {
+    for (let j = i; j >= min; j--) {
+      const product = i * j;
+      if (product <= maxPalindrome) {
+        break;
+      }
+      const productStr = product.toString();
+      if (productStr === productStr.split('').reverse().join('')) {
+        maxPalindrome = product;
+      }
+    }
+  }
+
+  return maxPalindrome;
 }
 
 largestPalindromeProduct(3);
