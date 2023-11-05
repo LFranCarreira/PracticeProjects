c7516ebe6b1c54df2d371541064973eb35d6c750
Franco Carreira <carreirafranco@gmail.com>
2023-11-05 14:36:06 -0300

    Solved Project Euler Problem #7: 10001st Prime

--- 0 to 20/Problem7_10001STPrime.js
+++ 0 to 20/Problem7_10001STPrime.js
@@ -3,8 +3,35 @@
 // What is the nth prime number?
 
 function nthPrime(n) {
-  //Primes array which will store all the prime numbers
-  const primes = [2];
+  if (n === 1) {
+    return 2; // The first prime number is 2
+  }
+
+  let primes = [2]; // Start with 2 as the first prime
+  let number = 3; // Start checking for primes from 3
+
+  while (primes.length < n) {
+    let isPrime = true;
+
+    // Check if the current number is prime
+    for (let prime of primes) {
+      if (prime * prime > number) {
+        break; // No need to continue checking
+      }
+      if (number % prime === 0) {
+        isPrime = false;
+        break; // It's not prime, move to the next number
+      }
+    }
+
+    if (isPrime) {
+      primes.push(number);
+    }
+
+    number += 2; // Only check odd numbers (even numbers other than 2 are not prime)
+  }
+
+  return primes[n - 1]; // Return the nth prime
 }
 
 nthPrime(10001);
