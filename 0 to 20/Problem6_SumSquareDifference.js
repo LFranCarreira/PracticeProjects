d80088176a1b6b4d71b3bdf305053c9a8b130387
Franco Carreira <carreirafranco@gmail.com>
2023-11-04 23:08:07 -0300

    Solved Project Euler Problem #5: Sum Square Difference

--- 0 to 20/Problem6_SumSquareDifference.js
+++ 0 to 20/Problem6_SumSquareDifference.js
@@ -8,6 +8,10 @@
 
 // Find the difference between the sum of the squares of the first n natural numbers and the square of the sum.
 
-function sumSquareDifference(n) {}
+function sumSquareDifference(n) {
+  const sumOfN = (n * (n + 1)) / 2;
+  const sumOfNSquare = (n * (n + 1) * (2 * n + 1)) / 6;
+  return sumOfN ** 2 - sumOfNSquare;
+}
 
 sumSquareDifference(100);
