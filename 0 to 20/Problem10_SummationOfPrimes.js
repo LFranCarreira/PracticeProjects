7a7a3cfb8f45006312df32dc203dbb8dd1fcda65
Franco Carreira <carreirafranco@gmail.com>
2023-11-06 15:29:33 -0300

    Problem 10: Summation of primes; example outputs

--- 0 to 20/Problem10_SummationOfPrimes.js
+++ 0 to 20/Problem10_SummationOfPrimes.js
@@ -1,3 +1,15 @@
 // The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 // Find the sum of all the primes below n.
+
+function primeSummation(n) {
+  return true;
+}
+
+primeSummation(2000000);
+
+// primeSummation(17) should return a number.
+// primeSummation(17) should return 41.
+// primeSummation(2001) should return 277050.
+// primeSummation(140759) should return 873608362.
+// primeSummation(2000000) should return 142913828922.
