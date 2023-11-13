75fd5d57bda151d4a80fa18485631b8c164d7356
Franco Carreira <carreirafranco@gmail.com>
2023-11-13 16:59:20 -0300

    Solved problem 2 With examples

--- Other excercises/Problem2_InventoryUpdate.js
+++ Other excercises/Problem2_InventoryUpdate.js
@@ -22,3 +22,18 @@ function updateInventory(currentInventory, newInventory) {
     return 0;
   });
 }
+var curInv = [
+  [21, 'Bowling Ball'],
+  [2, 'Dirty Sock'],
+  [1, 'Hair Pin'],
+  [5, 'Microphone'],
+];
+
+var newInv = [
+  [2, 'Hair Pin'],
+  [3, 'Half-Eaten Apple'],
+  [67, 'Bowling Ball'],
+  [7, 'Toothpaste'],
+];
+
+updateInventory(curInv, newInv);
