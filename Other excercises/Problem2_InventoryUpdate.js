a3ccc8fa9c8d0ed00dbb879fae788a3afdd749f3
Franco Carreira <carreirafranco@gmail.com>
2023-11-13 16:58:39 -0300

    Solved Problem 2 Inventory Update

--- Other excercises/Problem2_InventoryUpdate.js
+++ Other excercises/Problem2_InventoryUpdate.js
@@ -1,1 +1,24 @@
 // Compare and update the inventory stored in a 2D array against a second 2D array of a fresh delivery. Update the current existing inventory item quantities (in arr1). If an item cannot be found, add the new item and quantity into the inventory array. The returned inventory array should be in alphabetical order by item.
+function updateInventory(currentInventory, newInventory) {
+  // Check each item of new inventory
+  for (let newItem of newInventory) {
+    let found = false;
+    // Check against current inventory
+    for (let oldItem of currentInventory) {
+      // Update value if new item is found
+      if (newItem[1] === oldItem[1]) {
+        oldItem[0] += newItem[0];
+        found = true;
+        break;
+      }
+    }
+    // Otherwise add item to the old inventory
+    if (!found) currentInventory.push([...newItem]);
+  }
+  // Alphabetize and put into original array format
+  return currentInventory.sort((a, b) => {
+    if (a[1] < b[1]) return -1;
+    if (a[1] > b[1]) return 1;
+    return 0;
+  });
+}
