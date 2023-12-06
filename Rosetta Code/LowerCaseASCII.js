b4e7119f880e08497b24f8203e9c685815550469
Franco Carreira <carreirafranco@gmail.com>
2023-12-05 21:28:51 -0300

    lower_case_ASCII_finished

--- Rosetta Code/LowerCaseASCII.js
+++ Rosetta Code/LowerCaseASCII.js
@@ -1,3 +1,14 @@
 // Write a function to generate an array of lower case ASCII characters for a given range. For example, given the range ['a', 'd'], the function should return ['a', 'b', 'c', 'd'].
 
-function lascii(cFrom, cTo) {}
+function lascii(cFrom, cTo) {
+  // Initiate array to store letters
+  const letters = [];
+  // Store ascii values in two variables
+  let codeFrom = cFrom.charCodeAt(0);
+  let codeTo = cTo.charCodeAt(0);
+  // Push the letters coresponding to ascii values
+  for (let i = codeFrom; i <= codeTo; i++) {
+    letters.push(String.fromCharCode(i));
+  }
+  return letters;
+}
