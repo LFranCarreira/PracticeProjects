a523023c805dbe7fbbb46dceb6fd777934602fc0
Franco Carreira <carreirafranco@gmail.com>
2024-01-16 19:28:16 -0300

    return case

--- Hacker rank excercises/buildTheSubsequence.js
+++ Hacker rank excercises/buildTheSubsequence.js
@@ -1,4 +1,10 @@
-function buildSubsequences(s) {}
+// Build The Subsequences should return all the subsequences of a string in an array full of strings
+function buildSubsequences(s) {
+  // Return case
+  if (!s.length) {
+    return;
+  }
+}
 
 buildSubsequences('xyz');
 // The output should be ["x", "xy", "xyz", "xz", "y", "yz", "z"]
