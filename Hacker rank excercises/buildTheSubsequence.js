eae7540033b744b61bf74b8b68478997f6db0952
Franco Carreira <carreirafranco@gmail.com>
2024-01-16 20:01:02 -0300

    Finished excercise

--- Hacker rank excercises/buildTheSubsequence.js
+++ Hacker rank excercises/buildTheSubsequence.js
@@ -2,9 +2,26 @@
 function buildSubsequences(s) {
   // Return case
   if (!s.length) {
-    return;
+    return [];
   }
+
+  // Recur with the string excluding the first character
+  const subseqWithoutFirst = buildSubsequences(s.slice(1));
+
+  // Include the first character in each subsequence
+  const subseqWithFirst = subseqWithoutFirst.map((subseq) => s[0] + subseq);
+
+  // Combine both sets of subsequences and include the empty string
+  return [...subseqWithFirst, ...subseqWithoutFirst, s[0]];
 }
 
-buildSubsequences('xyz');
+// function getAllSortedSubsequences(s) {
+//   const allSubsequences = buildSubsequences(s);
+//   return allSubsequences.filter((subseq) => subseq !== '').sort();
+// }
+
+// Example usage:
+const inputString = 'xyz';
+const result = buildSubsequences(inputString);
+console.log(result);
 // The output should be ["x", "xy", "xyz", "xz", "y", "yz", "z"]
